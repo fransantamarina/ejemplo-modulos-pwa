5c2b58f59610d459c7fa8bb4b88af6fe661f72b5
Francisco Santamarina <fransantamarina@gmail.com>
2020-10-18 10:16:17 +0200

    Added sign and verify token and tested them on index routes

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 
+const indexRouter= require('./routes/index')
 const usersRouter = require('./routes/users')
 
 const app = express();
@@ -11,5 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
+app.use('/', indexRouter);
 
 module.exports = app;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
+const { createToken, verifyToken } = require('../utils/token')
+
+
+//TESTEO DE FUNCIONAMIENTO CORRECTO DE CREACION Y VERIFICACION DE TOKEN -> HACER REFACTOR Y COLOCAR DONDE CORRESPONDE
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/token', function (req, res, next) {
+  const payload = "1234";
+  const token = createToken({ payload })
+  res.json({ token: token })
+
+});
+router.get('/token/verify', function (req, res, next) {
+  const token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjoiMTIzNCIsImlhdCI6MTYwMzAwODI4NSwiZXhwIjoxNjAzMDExODg1fQ.c4qOzUTnQYqXH5cloCpgvbjvLNGTO8XqVa8WzrrznPYQwM7unIOgsiOx-Enr0peKqmm5AXslp0Njstwc8CXZygNgmND1VPfwL-CQqO9ItkVMtsQjd_1-gvr_1TE8bPyE4alCgY5pd_u27F-YnouvytyknhmJEAfRsnsiaQoHEL4"
+  const verifiedToken = verifyToken(token)
+  res.json({ verifiedToken })
 });
 
 module.exports = router;
