102195e18a862191c5db1bd77de9985280eef21b
Francisco Santamarina <fransantamarina@gmail.com>
2020-10-18 09:33:22 +0200

    Made general changes to make the app run correctly

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 /* POST users registration. */
-router.post('/', register)
+// router.post('/', register)
 
 module.exports = router;
