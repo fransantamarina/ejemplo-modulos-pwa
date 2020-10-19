fb7c611183020280f5cd8de3569c35ef919471fd
Francisco Santamarina <fransantamarina@gmail.com>
2020-10-20 01:17:03 +0200

    Corrección de rutas para centralizarlas todas en index

--- app/app.js
+++ app/app.js
@@ -2,16 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const indexRouter= require('./routes/index')
-const usersRouter = require('./routes/userRoutes')
-
 const app = express();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', usersRouter);
-app.use('/', indexRouter);
+app.use('/api', indexRouter);
 
 module.exports = app;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { sendEmail } = require("../utils/mailSender")
 
 
+const userRoutes = require('../routes/userRoutes');
+
+router.use('/users', userRoutes);
 
 
-//TESTEO DE FUNCIONAMIENTO CORRECTO DE CREACION Y VERIFICACION DE TOKEN -> HACER REFACTOR Y COLOCAR DONDE CORRESPONDE
-/* GET home page. */
-router.get('/token', function (req, res, next) {
-  const payload = "1234";
-  const token = createToken({ payload })
-  const email  = sendEmail({to: "fransantamarina@gmail.com", subject: "Espero que llegue el token", token})  
-  res.json(email)
-});
-router.get('/token/verify?', function (req, res, next) {
-  const token = req.query.token
-  const verifiedToken = verifyToken(token)
-  res.json({ verifiedToken })
-});
 
 module.exports = router;
