494166b6448d093143fde3727eeb62a0eae64f43
Francisco Santamarina <fransantamarina@gmail.com>
2020-10-27 18:20:01 +0100

    Fixed deletion of index route

---
+++ app/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+const userRoutes = require('../routes/userRoutes');
+
+router.use('/users', userRoutes);
+
+
+
+module.exports = router;
